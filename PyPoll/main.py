dcb6ae64872946a8a740e58f04c93409fa893d1a
Madison Fisher <mbfishe2@ncsu.edu>
2021-03-19 19:25:52 -0400

    found candidates and how many votes they each have

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,6 +9,11 @@ pollinfo = os.path.join("resources", "election_data.csv")
 
 #set variables to 0
 count = 0
+votes = 0
+
+#set list
+candidates = []
+votes_count = []
 
 #create csv reader
 with open(pollinfo, 'r') as csvfile:
@@ -16,15 +21,39 @@ with open(pollinfo, 'r') as csvfile:
     csv_reader= csv.reader(csvfile, delimiter=',')
     #skipping reading the top row
     header = next(csv_reader)
-    print(header)
 
     #run for each row in the data file
     for row in csv_reader:
         #to count each row aka each votes
         count = count + 1
-        
-        
+        #find all the names in the file
+        #set the variable to the candidate column
+        name = row[2]
+        #determine if the name is already in the list
+        if name not in candidates:
+            #if its not then add it to the list 
+            candidates.append(name)
+        #find how many candidates and put it in a range
+        number_can = range(len(candidates))
 
+for x in number_can:
+    votes_count.append(0)
+
+#recreate csv reader to run through file from the top
+with open(pollinfo, 'r') as csvfile:
+    #setting csv reader to be seperated by commas
+    csv_reader= csv.reader(csvfile, delimiter=',')
+    #skipping reading the top row
+    header = next(csv_reader)
+
+    #run for each row in the data file
+    for row in csv_reader:
+        for x in number_can:
+            if candidates[x] == row[2]:
+                votes_count[x] += 1
+    
+    print(candidates)
+    print(votes_count)
 
 
     #print title of output
